5978cee2bb20e0305cb4813e7f4565cb04697742
RonLi5321 <115131348+RonLi5321@users.noreply.github.com>
2023-07-26 21:41:17 -0700

    Updated Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,2 +1,32 @@
 import React from "react";
 
+export default function weather() {
+  return (
+    <div className="weather-app">
+      <div className="container">
+        <div className="card">
+          <div className="row">
+          </div>
+          <h2>San Francisco</h2>
+          <div>
+            <li>
+              <ul>Last Updated: Thursday at 18:00</ul>
+              <ul>Sunny</ul>
+            </li>
+          </div>
+          <div>
+            <h1>66</h1>
+            <div>
+              <span className="f-temp">°F</span>
+              <span className="c-temp">°C</span>
+              <li>
+                <ul>Humidity: 71%</ul>
+                <ul>Wind: 14 MPH</ul>
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
