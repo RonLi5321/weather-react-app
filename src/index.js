dd0adda1f062692402c381c20102f14dd81b6fd6
RonLi5321 <115131348+RonLi5321@users.noreply.github.com>
2023-07-26 22:21:47 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+import { StrictMode } from "react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<StrictMode>
