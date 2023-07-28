74024e3e8520fb0bd69b97723e9b624ff99eb4dd
RonLi5321 <115131348+RonLi5321@users.noreply.github.com>
2023-07-28 16:37:54 -0700

    Imported bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import Weather from "./Weather";
 import reportWebVitals from './reportWebVitals';
 
